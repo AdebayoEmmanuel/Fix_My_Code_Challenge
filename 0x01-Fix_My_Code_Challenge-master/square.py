4c5a301faa05c2aaadfa61c7d23f2a23e6667448
Emmanuel (Demilade) Adebayo <adebayodemilade7@gmail.com>
2023-01-27 14:30:46 +0100

    Update square.py

--- 0x01-Fix_My_Code_Challenge-master/square.py
+++ 0x01-Fix_My_Code_Challenge-master/square.py
@@ -6,13 +6,13 @@ class square():
     height = 0
 
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
